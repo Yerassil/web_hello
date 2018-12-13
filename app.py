56dc3ef4521cea0b90ce77035f4d84aae70be8e5
Your Name <you@example.com>
2018-12-13 23:51:16 +0600

    Add hello.html template

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello world!"
-
-
-@app.route("/bye")
+@app.route('/bye')
 def bye():
     return "Bye world!"
+
+
+@app.route('/')
+def hola():
+    return render_template('hello.html')
